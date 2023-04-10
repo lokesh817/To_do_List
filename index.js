beb89eec8faf1ad6ad903750cf5cda0cf6cb2db2
lokesh817 <lokeshkumarparihar143@gmail.com>
2023-04-10 22:13:18 +0530

    basic layout and database is set up

--- index.js
+++ index.js
@@ -8,30 +8,45 @@ const app=express();
 const db=require('./config/mongoose');
 const list_schema=require('./models/list_schema');
 
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
 app.get('/',function(req,res){
     list_schema.find()
     .then(function(ToDoList){
         return res.render('home',{
             title:'Home',
-            list:list_schema
+            To_Do_List:ToDoList
         });
     })
     .catch(function(err){
         console.log("error",err);
     })
 });
-app.post('/create-task',function(req,res){
+app.post('/create-task/',function(req,res){
     list_schema.create({
         Discription:req.body.Discription,
         Category:req.body.Category,
         Due_Date:req.body.Due_Date
     });
+    return res.redirect('back');
+})
+app.get('/delete-task',function(req,res){
+    const id=req.query.id;
+    list_schema.findByIdAndDelete(id)
+    .then(function(){
+        console.log('deleted task');
+        return res.redirect('back');
+    })
+    .catch(function(err){
+        console.log('not deleted');
+        return res.redirect('back');
+    })
 })
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded());
-app.use(express.static('./assets'));
+
 
 app.listen(port,function(err){
     if(err){
